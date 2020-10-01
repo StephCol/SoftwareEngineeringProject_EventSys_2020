fdcddffd62274c6a3f35e7633cdc3e9f5fd8b18e
StephCol <55181813+StephCol@users.noreply.github.com>
2020-10-01 14:18:06 +0100

    Update DBConnect.cs

--- EventSys/DBConnect.cs
+++ EventSys/DBConnect.cs
@@ -12,8 +12,6 @@ namespace EventSys
         //Use at home
         public const String oradb = " Data Source = localhost/Steph; User Id = hr ; Password = hr;";
 
-        //Use in College
-        //public const String oradb = " Data Source = oracle/orcl; User Id = T00208279; Password = 9cpa3mwe;";
 
 
     }
